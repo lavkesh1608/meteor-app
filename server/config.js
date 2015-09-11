c9a2fd9c60f6daa920cea1136ada29f045b58434
Lavkesh agrawal <lavkesh1608@gmail.com>
2015-09-11 13:03:32 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -4,8 +4,8 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
 	service : "github",
-	clientId : "8839cb98d8b1e3c44fa3",
-	secret : "1c9edc249385722992c55504ced1d0ec5e176713"
+	clientId : "XXXXXXXXXXXXXXXX",
+	secret : "XXXXXXXXXXXXXXXXXXXX"
 });
 
 ServiceConfiguration.configurations.remove({
@@ -14,6 +14,6 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
 	service : "twitter",
-	consumerKey : "Eywbacphmv51F3XDDhCLJFfYV",
-	secret : "j8uYwIK8h88H6RzR8GUg7k92fWhyAy43Iifqgbyt7TQZuZITIC"
+	consumerKey : "XXXXXXXXXXXXXXXX",
+	secret : "XXXXXXXXXXXXXXXXXXXX"
 });
